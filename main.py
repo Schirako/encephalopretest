41ed2a6fb8ab4d604c37c57c2b52bfa61756e9af
Schirako <62351299+Schirako@users.noreply.github.com>
2020-03-18 19:32:33 -0400

    Updated main.py
    I realized that I did it incorrectly during the exam and I know that this won't change the results of the exam, I just wanted to fix my mistake and attempt to complete it properly. The top 10 companies based on market cap are as follows:
    67      PFE
    110      PM
    205     MMM
    13     ABBV
    137      TD
    280    PCLN
    265     WBK
    291     LOW
    159    PYPL
    66      AMT

--- main.py
+++ main.py
@@ -6,12 +6,20 @@ def main():
     # main function.
     # Return the top 10 companies with the highest market cap.
     data = pd.read_csv("./data.csv") # read csv using pandas
-    
-    data_market_cap = data.sort_values(by = 'market_cap', ascending = False)
-    
-    return data_market_cap.iloc[0:10,0]
-    
+    data['market_cap'] = data['market_cap'].str.replace('$','')
 
+    def to_float(x):
+        if type(x) == float or type(x) == int:
+            return x
+        if 'M' in x:
+            return float(x.replace('M', '')) * 1000000
 
+        if 'B' in x:
+            return float(x.replace('B', '')) * 1000000000
+
+    data.market_cap = data.market_cap.apply(to_float)
+
+    market_cap = data.sort_values(by = 'market_cap',ascending = False).head(10)
+    return market_cap['ticker']
 if __name__ == "__main__":
     main()
