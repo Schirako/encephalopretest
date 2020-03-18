2c6dfd9b45c32e83a305600caa8e86296454ed1c
Schirako <62351299+Schirako@users.noreply.github.com>
2020-03-18 18:19:23 -0400

    Updated main.py

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ def main():
     # main function.
     # Return the top 10 companies with the highest market cap.
     data = pd.read_csv("./data.csv") # read csv using pandas
+    
+    data_market_cap = data.sort_values(by = 'market_cap', ascending = False)
+    
+    return data_market_cap.iloc[0:10,0]
+    
 
 
 if __name__ == "__main__":
